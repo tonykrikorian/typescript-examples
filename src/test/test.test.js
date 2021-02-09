ee7d84d248be0b5408ac28b114d1b4a0c6b0079f
tkrikoriam <tkrikoriam@achs.cl>
2021-02-09 09:55:59 -0300

    Add mocha unit testing

---
+++ src/test/test.test.js
@@ -0,0 +1,17 @@
+/**
+ * La funcion describe se usa para agrupar tests
+ */
+describe("class Math", () => {
+  // Cuando se anida describe es para sacar subgrupos o partes de una funcionalidad o clase
+  describe("Sum Method", () => {
+    it("Debe hacer la suma de 2 o N numeros", () => {});
+    it("Debe Devolver cero cuando no hay argumentos", () => {});
+    // it("Debe fallar cuando se pasan valores que no corresponden", () => {
+    //   throw new Error("Errror al dividir por cero");
+    // });
+  });
+
+  describe("Less Method", () => {});
+
+  describe("Multiply Method", () => {});
+});
